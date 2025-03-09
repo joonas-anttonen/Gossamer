2957350a8fc1434efd5380466b3ed7381e9108ac
Joonas Anttonen <joonas.anttonen@outlook.com>
2025-03-09 16:26:40 +0200

    Gui: Rectangle debugging and added some new convenience functions

--- Gossamer/Rectangle.cs
+++ Gossamer/Rectangle.cs
@@ -19,9 +19,12 @@ public readonly record struct Rectangle(float Left, float Top, float Right, floa
     public bool Contains(Vector2 p) => (Left <= p.X) && (Top <= p.Y) && (Right >= p.X) && (Bottom >= p.Y);
 
     public Rectangle CenterOn(Vector2 p) => FromXYWH(p.X - Width / 2f, p.Y - Height / 2f, Width, Height);
+    public Rectangle CenterOn(Rectangle r) => FromXYWH(r.Center.X - Width / 2f, r.Center.Y - Height / 2f, Width, Height);
     public Rectangle Crop(float l, float t, float r, float b) => new(Left + l, Top + t, Right - r, Bottom - b);
+    public Rectangle Scale(Vector2 v) => new(Left, Top, Left + Width * v.X, Top + Height * v.Y);
     public Rectangle Scale(float x, float y) => new(Left, Top, Left + Width * x, Top + Height * y);
-    public Rectangle Move(float x, float y) => new(Left + x, Top + y, Right + x, Bottom + y);
+    public Rectangle Move(Vector2 v) => FromXYWH(Left + v.X, Top + v.Y, Width, Height);
+    public Rectangle Move(float x, float y) => FromXYWH(Left + x, Top + y, Width, Height);
     public Rectangle Clamp(Rectangle other) => new(MathF.Max(Left, other.Left), MathF.Max(Top, other.Top), MathF.Min(Right, other.Right), MathF.Min(Bottom, other.Bottom));
 
     public static Rectangle FromPositionSize(Vector2 position, Vector2 size) => new(position.X, position.Y, position.X + size.X, position.Y + size.Y);
